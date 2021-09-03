e5133f14a43134003bfd23fa21b691b5bbb1ebcc
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-09-03 18:59:33 +0300

    Create javascript to send information from form to table

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var modal = document.getElementById('modal-window');
 var modalBtn = document.getElementById('modal-button');
 var closeBtn = document.getElementsByClassName("close-button")[0];
-
+var submitBtn = document.getElementById('submit-button');
 modalBtn.addEventListener('click', openModal);
 
 function openModal() {
@@ -14,10 +14,42 @@ function closeModal() {
     modal.style.display= 'none';
 }
 
+submitBtn.addEventListener('click',closeModal);
+
 window.addEventListener('click', clickOutside);
 
 function clickOutside(e) {
     if(e.target == modal) {
         modal.style.display= 'none';
     }
+}
+
+function onCreate() {
+    var formData = readFormData();
+    insertNewRecord(formData);
+}
+
+function readFormData() {
+    var formData = {};
+    formData["name"] = document.getElementById("name").value;
+    formData["surname"] = document.getElementById("surname").value;
+    formData["email"] = document.getElementById("email").value;
+    formData["gender"] = document.getElementById("gender").value;
+    formData["birthday"] = document.getElementById("birthday").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow();
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.name;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.surname;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.email;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.gender;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.birthday;
 }
