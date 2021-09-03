2c89e2429216af76b15f5852c7be02468fe91cdb
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-09-03 13:02:14 +0300

    Create java script to close modal window on button press

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ var modal = document.getElementById('modal-window');
 //Open modal button
 var modalBtn = document.getElementById('modal-button');
 //Close modal button
-var closeBtn = document.getElementById("close-button");
+var closeBtn = document.getElementsByClassName("close-button")[0];
 
 modalBtn.addEventListener('click', openModal);
 
 function openModal() {
     modal.style.display= 'block';
+}
+
+closeBtn.addEventListener('click', closeModal);
+
+function closeModal() {
+    modal.style.display= 'none';
 }
